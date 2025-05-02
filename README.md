fa21cc4deaa3eea7ac2fc07fdea7cebc80a1bfc9
Robin Mordasiewicz <robin@mordasiewicz.com>
2025-05-02 15:11:40 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quickstart: Use Terraform to create a Linux VM
+
+(Reference)[https://learn.microsoft.com/en-us/azure/virtual-machines/linux/quick-create-terraform?tabs=azure-cli]
+
+az vm image list --publisher Canonical
---
+++ outputs.tf
@@ -0,0 +1,28 @@
+output "resource_group_name" {
+  description = "The name of the resource group where the VM is deployed."
+  value = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  description = "The public IP address of the Linux VM."
+  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+}
+
+output "ssh_config_instructions" {
+  description = "Instructions to set up SSH configuration for the VM."
+  value = <<EOF
+
+1. Create the following file ~/.ssh/config with the content:
+
+Host devcontainer
+  HostName ${azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address}
+  User vscode
+  ForwardAgent yes
+  IdentityFile ~/.ssh/devcontainer
+
+2. Create the file ~/.ssh/devcontainer and save the private key using:
+
+terraform output -raw key_data > ~/.ssh/devcontainer
+
+EOF
+}
